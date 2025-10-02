e466e84423fb67a29cb9d750f757c3bd478ff076
Raniere Gaia Costa da Silva <Raniere.CostadaSilva@gesis.org>
2025-10-02 12:27:50 +0200

    Revisit noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -1,26 +1,35 @@
-"""A nox configuration file so that we can build the documentation easily with nox.
-- see the README.md for information about nox.
-- ref: https://nox.thea.codes/en/stable/
-"""
 import nox
 
 nox.options.reuse_existing_virtualenvs = True
 
-build_command = ["-b", "dirhtml", "docs", "docs/_build/html"]
 
-@nox.session()
+@nox.session(python="3.13", default=False)
 def docs(session):
+    """
+    Build the documentation.
+    """
+    doc_build_default_args = ["-b", "dirhtml", "docs", "docs/_build/html"]
+
     session.install("-r", "docs/requirements.txt")
-    if "live" in session.posargs:
+
+    if "live" in session.posargs or (
+        session.interactive and "only-build" not in session.posargs
+    ):
+        # For live preview, sphinx-autobuild is used.
+        # To avoid sphinx-autobuild be missing,
+        # sphinx-autobuild is installed explicitly.
+        session.install("sphinx-autobuild")
+        cmd = ["sphinx-autobuild"]
+
         # Add relative paths to this if we ever need to ignore them
         AUTOBUILD_IGNORE = [
-          "docs/_data",
-          "docs/_build",
+            "docs/_build",
         ]
-        cmd = ["sphinx-autobuild"]
+
         for folder in AUTOBUILD_IGNORE:
             cmd.extend(["--ignore", f"*/{folder}/*"])
-        cmd.extend(build_command)
+
+        cmd.extend(doc_build_default_args)
         session.run(*cmd)
     else:
-        session.run("sphinx-build", *build_command)
+        session.run("sphinx-build", *doc_build_default_args)
